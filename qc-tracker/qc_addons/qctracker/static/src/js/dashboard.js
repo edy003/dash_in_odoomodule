bd772757493f78ecc119a57c356c775fdca6f4cc
edy003 <97347567+edy003@users.noreply.github.com>
2025-04-08 00:31:20 +0100

    Update dashboard.js

--- qc-tracker/qc_addons/qctracker/static/src/js/dashboard.js
+++ qc-tracker/qc_addons/qctracker/static/src/js/dashboard.js
@@ -5,21 +5,17 @@ odoo.define('qctracker.dashboard', function (require) {
     var core = require('web.core');
     
     var QCTrackerDashboard = AbstractAction.extend({
-        // Ne pas spécifier de template
-        
-        /**
-         * @override
-         */
         start: function () {
             var self = this;
             
-            // Créer l'iframe directement
+            // Créer l'iframe avec un conteneur approprié
             this.$el.empty().addClass('qctracker_dashboard_container').css({
                 'width': '100%',
                 'height': '100%',
                 'padding': '0',
                 'margin': '0',
-                'overflow': 'hidden'
+                'overflow': 'hidden',
+                'position': 'relative'
             });
             
             var iframe = $('<iframe>', {
@@ -32,53 +28,73 @@ odoo.define('qctracker.dashboard', function (require) {
                     'border': 'none',
                     'display': 'block',
                     'margin': '0',
-                    'padding': '0'
+                    'padding': '0',
+                    'background-color': '#ffffff'
                 }
             });
             
             this.$el.append(iframe);
             
-            // Ajout du CSS global pour corriger les problèmes de layout
+            // CSS moins agressif qui préserve les menus
             $('<style>')
                 .text(`
-                    .o_action_manager {
-                        margin: 0 !important;
+                    /* Maximiser la zone de contenu tout en préservant le menu principal */
+                    .o_content {
                         padding: 0 !important;
+                        margin: 0 !important;
+                        border: 0 !important;
+                        height: calc(100vh - 46px) !important;
+                        width: 100% !important;
                         overflow: hidden !important;
+                        background-color: #ffffff !important;
                     }
                     
+                    /* Supprimer seulement les contrôles spécifiques, pas le menu principal */
                     .o_control_panel, .o_cp_buttons, .o_cp_left, .o_cp_right,
                     .o_form_buttons_view, .o_form_buttons_edit, .breadcrumb {
                         display: none !important;
                     }
                     
-                    .o_content {
-                        padding: 0 !important;
-                        margin: 0 !important;
-                        height: calc(100vh - 46px) !important;
+                    /* Assurer l'iframe prend tout l'espace */
+                    #dashboard_iframe {
                         width: 100% !important;
-                        position: absolute !important;
-                        top: 46px !important;
-                        left: 0 !important;
-                        right: 0 !important;
+                        height: 100% !important;
+                        border: none !important;
+                        margin: 0 !important;
+                        padding: 0 !important;
+                        background-color: #ffffff !important;
+                    }
+                    
+                    /* Container de l'iframe */
+                    .qctracker_dashboard_container {
+                        width: 100% !important;
+                        height: 100% !important;
+                        position: relative !important;
                         overflow: hidden !important;
                     }
                 `)
                 .appendTo('head');
             
-            // Fonction d'ajustement
+            // Ajustement des dimensions sans position fixed
             function adjustDimensions() {
-                var headerHeight = $('.o_main_navbar').outerHeight() || 46;
+                var headerHeight = $('.o_main_navbar').outerHeight(true) || 46;
                 
+                // Ajuster la hauteur du contenu en fonction de la hauteur du menu
                 self.$el.parents('.o_content').css({
                     'height': 'calc(100vh - ' + headerHeight + 'px)',
-                    'top': headerHeight + 'px'
+                    'width': '100%'
+                });
+                
+                // S'assurer que l'iframe occupe tout l'espace
+                $('#dashboard_iframe').css({
+                    'width': '100%',
+                    'height': '100%'
                 });
             }
             
             // Appliquer les ajustements
-            setTimeout(adjustDimensions, 100);
-            setTimeout(adjustDimensions, 500);
+            adjustDimensions();
+            setTimeout(adjustDimensions, 250);
             
             // Réajuster lors du redimensionnement
             $(window).on('resize', adjustDimensions);
@@ -86,9 +102,6 @@ odoo.define('qctracker.dashboard', function (require) {
             return this._super.apply(this, arguments);
         },
         
-        /**
-         * @override
-         */
         destroy: function () {
             $(window).off('resize');
             this._super.apply(this, arguments);
